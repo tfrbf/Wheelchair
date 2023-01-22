cb6bbc414d89974acf45b30bc13f6cb06a2b3064
Ata Dibazar <118345682+Atadbz@users.noreply.github.com>
2023-01-22 14:20:03 +0330

    Update 2motor.ino

--- Codes/2motor.ino
+++ Codes/2motor.ino
@@ -1,104 +1,109 @@
-#define _60secInMicrisec 60000000.0f
-#define GearRatio 34.0f
-#define EncoderCPR 360
-#define pi 3.1415f
-int ENA = 10;  // to EnA of L298
-int ENB = 11;
-int IN1 = 9;  // to IN1 of L298
-int IN2 = 8;  // to IN2 of L298
-int IN3 = 13;
-int IN4 = 12;
+#include <string.h>
 
-const byte ChA = 2;  // to ENA (OutA / ChA) of Encoder
-const byte ChB = 3;  // to ENB (OutA / ChA) of Encoder
-const byte ChC = 4;
-const byte ChD = 5;
-
-volatile unsigned long _1RevDuration;
-volatile unsigned long _1RevDuration2;  // Duration of 1 Revolution
-float rpm = 0;                          // Motor Speed
-float rpm2 = 0;
-volatile int feedbackDirection;  // Direction Detected by Encoder
-volatile int feedbackDirection2;
-enum desiredDirection 
-{// Desired Direction
-  CW = 0,           // Clockwise
-  CCW = 1         // Counter Clockwise
-};
-
-float pwm;         // PWM to L298
-long counter = 0;  // Counter for Sinusoidal PWM Generation
-
-void setPWM(float outputPWM);             // Apply PWM to motor (input between 0-100)
-void setDirection(desiredDirection dir);  // Select Direction of Rotation
+int enA = 10;
+int in1 = 9;
+int in2 = 8;
+int enB = 11;
+int in3 = 13;
+int in4 = 12;
+char serial_data;
 
 void setup() {
-  Serial.begin(1200);
-  pinMode(ENA, OUTPUT);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(ENB, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  setDirection(CW);                                                     // set Initial Diretion to Clockwise
-  setPWM(90);                                                           // initialize Motor
-  attachInterrupt(digitalPinToInterrupt(ChA), checkDirection, RISING);  // Define Pin ChA as Intrrupt Pin
+  Serial.begin(9600);
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+  Serial.print("Connected  to e-two\n");
+
 }
 
+
+
+
+
+//////////////////////////////////////////////////////
 void loop() {
-  pwm = 80.0f * sin(2.0f * pi * counter / 100.0f);
-  counter += 1;
-  if (pwm > 0)
-    setDirection(CW);
-  else
-    setDirection(CCW);
-  setPWM(fabs(pwm));
-  //delay(100);
-  _1RevDuration = pulseIn(ChA, LOW, 10000) * EncoderCPR * 2;
-  _1RevDuration2 = pulseIn(ChC, LOW, 10000) * EncoderCPR * 2;  // Calculating the Duration of a Revolution in micro seconds
-  if (_1RevDuration == 0)                                      // Avoiding from Singularity Problem
-    rpm = 0;
-
-  else
-    rpm = feedbackDirection * _60secInMicrisec / ((float)_1RevDuration * GearRatio);  // Calculating Motor RPM
-
-  if (_1RevDuration2 == 0)  // Avoiding from Singularity Problem
-    rpm2 = 0;
-
-  else
-    rpm2 = feedbackDirection2 * _60secInMicrisec / ((float)_1RevDuration2 * GearRatio);
-  Serial.print("Motor1: ");
-  Serial.print(rpm);
-  Serial.print("   ");
-  //Serial.println("\n");
-
-  Serial.print("Motor2: ");
-  Serial.print(rpm2);
-  Serial.print("   ");
-  Serial.println("\n");
+if (Serial.available()) {
+  serial_data = Serial.read();
+  Serial.println(serial_data);
+  }
+  
+  if (serial_data == '1')
+  demoone();
+  serial_data=0;
 }
 
-void setDirection(desiredDirection dir) {
-  switch (dir) {
-    case CW:
-      digitalWrite(IN1, LOW);
-      digitalWrite(IN2, HIGH);
-      digitalWrite(IN3, LOW);
-      digitalWrite(IN4, HIGH);
-      break;
-    case CCW:
-      digitalWrite(IN2, LOW);
-      digitalWrite(IN1, HIGH);
-      digitalWrite(IN4, LOW);
-      digitalWrite(IN3, HIGH);
-      break;
+
+
+
+
+
+
+
+void demoone() {
+  //Activate motor A
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  //analogWrite(enA, 200);  //set speed to 200
+
+  //Activate motor B
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  //analogWrite(enB, 200);  //set speed to 200
+
+
+for (int i = 0; i < 255; i++) {
+
+    analogWrite(enA, i);
+    analogWrite(enB, i);
+    delay(30);
+  }
+
+  // delay(2000);
+  // // تغییر مسیر موتورها
+  // digitalWrite(in1, LOW);   // غیر فعال شدن خروجی IN1
+  // digitalWrite(in2, HIGH);  // فعال شدن خروجی IN2
+  // digitalWrite(in3, LOW);   // غیر فعال شدن خروجی IN3
+  // digitalWrite(in4, HIGH);  // فعال شدن خروجی IN4
+  // delay(2000);              // تاخیر ۲ ثانیه
+  // // خاموش شدن هر دو موتور
+  // digitalWrite(in1, LOW);  // غیر فعال شدن
+  // digitalWrite(in2, LOW);  // غیر فعال شدن
+  // digitalWrite(in3, LOW);  // غیر فعال شدن
+  // digitalWrite(in4, LOW);  // غیر فعال شدن
+}
+
+
+void forward() {
+  // روشن شدن موتورها
+  digitalWrite(in1, LOW);   // غیر فعال شدن خروجی IN1
+  digitalWrite(in2, HIGH);  // فعال شدن خروجی IN2
+  digitalWrite(in3, LOW);   // غیر فعال شدن خروجی IN3
+  digitalWrite(in4, HIGH);  // فعال شدن خروجی IN4
+  // تعیین شتاب از ۰ تا ماکزیمم سرعت
+  for (int i = 0; i < 255; i++) {
+    analogWrite(enA, i);
+    analogWrite(enB, i);
+    delay(20);
   }
 }
-void setPWM(float outputPWM) {
-  analogWrite(ENA, outputPWM * 255 / 100);
-  analogWrite(ENB, outputPWM * 255 / 100);
-}
-void checkDirection() {
-  feedbackDirection = -2 * bitRead(PIND, ChB) + 1;
-  feedbackDirection2 = -2 * bitRead(PIND, ChD) + 1;
+
+
+void stop() {
+  digitalWrite(in1, LOW);  // غیر فعال شدن
+  digitalWrite(in2, LOW);  // غیر فعال شدن
+
+  digitalWrite(in3, LOW);  // غیر فعال شدن
+  digitalWrite(in4, LOW);  // غیر فعال شدن
+
+  // از ماکزیمم سرعت تا صفر
+  for (int i = 255; i >= 0; --i) {
+    analogWrite(enA, i);
+    analogWrite(enB, i);
+    delay(20);
+  }
 }
